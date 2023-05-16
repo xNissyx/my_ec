2918720b14b62387cb17e4e8f9326298a6f4358d
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-05-16 07:28:19 +0000

    [Add] Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'homes/top'
   devise_for :customers, controllers: {
    sessions: "customer/sessions",
    registrations: "customer/registrations"
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
   sessions: "admin/sessions"
 }
+
+  root to: "homes#top"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
